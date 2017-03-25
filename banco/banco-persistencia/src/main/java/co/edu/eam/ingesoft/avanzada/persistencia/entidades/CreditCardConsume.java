82c97258c80b6e1a3e81d8965c3b2db338a468cd
TOSHIBAP55W <TOSHIBAP55W@Kevin-Zapata>
2017-03-25 17:09:13 -0500

    act

--- banco/banco-persistencia/src/main/java/co/edu/eam/ingesoft/avanzada/persistencia/entidades/CreditCardConsume.java
+++ banco/banco-persistencia/src/main/java/co/edu/eam/ingesoft/avanzada/persistencia/entidades/CreditCardConsume.java
@@ -26,7 +26,6 @@ public class CreditCardConsume implements Serializable{
 
 	public static final String CONSUMOS_TARJETAS="Consumos.Tarjetas";
 	
-	//atributos de la clase
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
