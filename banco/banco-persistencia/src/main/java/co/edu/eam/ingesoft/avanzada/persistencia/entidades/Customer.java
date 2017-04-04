ed15ea53e001da677875017037f3279abaa0ea47
GAR-T <GAR-T@GART>
2017-04-04 08:24:38 -0500

    Agregacion de campos

--- banco/banco-persistencia/src/main/java/co/edu/eam/ingesoft/avanzada/persistencia/entidades/Customer.java
+++ banco/banco-persistencia/src/main/java/co/edu/eam/ingesoft/avanzada/persistencia/entidades/Customer.java
@@ -39,6 +39,12 @@ public class Customer implements Serializable{
 
 	@Column(name = "lastname", nullable = false,length=50)
 	private String lastName;
+	
+	@Column(name="correo")
+	private String correo;
+	
+	@Column(name="telefono")
+	private String telefono;
 
 	@OneToMany(mappedBy="customer",cascade={})
 	private List<Product> productos;
@@ -47,54 +53,123 @@ public class Customer implements Serializable{
 		// TODO Auto-generated constructor stub
 	}
 
-    public Customer(String idType, String idNum, String name, String lastName) {
-        this.idType = idType;
-        this.idNum = idNum;
-        this.name = name;
-        this.lastName = lastName;
-    }
+	public Customer(String idType, String idNum, String name, String lastName, String correo, String telefono,
+			List<Product> productos) {
+		super();
+		this.idType = idType;
+		this.idNum = idNum;
+		this.name = name;
+		this.lastName = lastName;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.productos = productos;
+	}
 
-    public String getIdType() {
-        return idType;
-    }
+	/**
+	 * @return the idType
+	 */
+	public String getIdType() {
+		return idType;
+	}
 
-    public void setIdType(String idType) {
-        this.idType = idType;
-    }
+	/**
+	 * @param idType the idType to set
+	 */
+	public void setIdType(String idType) {
+		this.idType = idType;
+	}
 
-
+	/**
+	 * @return the idNum
+	 */
 	public String getIdNum() {
 		return idNum;
 	}
 
+	/**
+	 * @param idNum the idNum to set
+	 */
 	public void setIdNum(String idNum) {
 		this.idNum = idNum;
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the lastName
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * @param lastName the lastName to set
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * @return the correo
+	 */
+	public String getCorreo() {
+		return correo;
+	}
+
+	/**
+	 * @param correo the correo to set
+	 */
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	/**
+	 * @return the productos
+	 */
 	public List<Product> getProductos() {
 		return productos;
 	}
 
+	/**
+	 * @param productos the productos to set
+	 */
 	public void setProductos(List<Product> productos) {
 		this.productos = productos;
 	}
-	
+
+	/**
+	 * @return the listaclientes
+	 */
+	public static String getListaclientes() {
+		return ListaClientes;
+	}
+
 	
 	
 	
